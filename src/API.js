d3c8af18a026a1efc0cb893c24d7b8d580ff8e94
Sammi A <sammialldredge@gmail.com>
2016-10-15 18:56:51 -0700

    apiiii

--- src/API.js
+++ src/API.js
@@ -4,10 +4,18 @@ import ServerActions from './actions/ServerActions'
 const API = {
   searchAddress(address){
     // console.log('key:', process.env.API_KEY)
-    get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyCoAuYhajAzi3Sn7ciZQVaUGe2-rYqN7bU`)
+    get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.API_KEY}`)
     .then(res => {
       console.log('res:', res);
       ServerActions.gotCoord(res.data.results[0].geometry.location)
+    })
+      .catch(console.error)
+  },
+  showCloseBy(coord){
+    get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coord}&radius=500&key=${process.env.API_KEY}`)
+    .then(res => {
+      console.log('res:', res);
+      ServerActions.gotNearBy(res.data)
     })
       .catch(console.error)
   }
